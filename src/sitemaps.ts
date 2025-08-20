92e9a7c693313bfabd9beb5dc749ff6b7ab88801
Jesus Arrioja <jarrioja2210@gmail.com>
2025-08-19 21:01:32 -0300

    feat: add default value for sitemapName parameter

--- src/sitemaps.ts
+++ src/sitemaps.ts
@@ -143,10 +143,14 @@ export const createSitemapIndexElement = (
     sitemapLoc: string,
     sitemapIndex: number,
     lastmod: Date,
-    sitemapName: string
+    sitemapName = 'sitemap'
 ) => {
     const sitemapEl = builder.create('sitemap');
-    sitemapEl.ele('loc', {}, `${sitemapLoc}/${sitemapName}.${sitemapIndex + 1}.xml`);
+    sitemapEl.ele(
+        'loc',
+        {},
+        `${sitemapLoc}/${sitemapName}.${sitemapIndex + 1}.xml`
+    );
     sitemapEl.ele('lastmod', {}, lastmod.toISOString());
     return sitemapEl;
 };
@@ -162,7 +166,9 @@ export const generateSitemapIndex = async (
     const root = builder.create('sitemapindex', { encoding: 'utf-8' });
     root.attribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
     for (let i = 0; i < sitemapCount; i++) {
-        root.children.push(createSitemapIndexElement(sitemapLoc, i, now, sitemapName));
+        root.children.push(
+            createSitemapIndexElement(sitemapLoc, i, now, sitemapName)
+        );
     }
     const finalOutput = root.end({ pretty: !minifyOutput });
     await writeFile(`${outputFolder}/${sitemapName}.xml`, finalOutput);
